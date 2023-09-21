5c8f4448999fb6673c6238c9397aec1387840f7c
eva <me@example.com>
2023-09-21 08:57:13 +0200

    created part shopping list

--- src/app/parts-shopping-list/part-list.component.ts
+++ src/app/parts-shopping-list/part-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Part } from '../shared/part.model';
 
 @Component({
   selector: 'app-part-list',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./part-list.component.css'],
 })
 export class PartListComponent implements OnInit {
-  bricks = [];
+  parts: Part[] = [
+    new Part(
+      '3298mia',
+      'Minitalia Slope 33° 3 x 2 with Bottom X Supports',
+      'https://cdn.rebrickable.com/media/parts/photos/2/3298mia-2-94cdd67a-5f98-463a-b440-46d183bb7222.jpg',
+      5
+    ),
+    new Part(
+      '3298mia',
+      'Minitalia Slope 33° 3 x 2 with Bottom X Supports',
+      'https://cdn.rebrickable.com/media/parts/photos/2/3298mia-2-94cdd67a-5f98-463a-b440-46d183bb7222.jpg',
+      5
+    ),
+  ];
   constructor() {}
   ngOnInit() {}
 }
--- src/app/shared/part.model.ts
+++ src/app/shared/part.model.ts
@@ -6,3 +6,28 @@ export class Part {
     public amount: number
   ) {}
 }
+
+// Response from API
+// {
+//   "count": 1,
+//   "next": null,
+//   "previous": null,
+//   "results": [
+//     {
+//       "part_num": "3298mia",
+//       "name": "Minitalia Slope 33° 3 x 2 with Bottom X Supports",
+//       "part_cat_id": 3,
+//       "part_url": "https://rebrickable.com/parts/3298mia/minitalia-slope-33-3-x-2-with-bottom-x-supports/",
+//       "part_img_url": "https://cdn.rebrickable.com/media/parts/photos/2/3298mia-2-94cdd67a-5f98-463a-b440-46d183bb7222.jpg",
+//       "external_ids": {
+//         "BrickLink": [
+//           "3298miA"
+//         ],
+//         "BrickOwl": [
+//           "683281"
+//         ]
+//       },
+//       "print_of": null
+//     }
+//   ]
+// }
